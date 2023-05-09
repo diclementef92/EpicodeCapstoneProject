133ef981185f79cc51171d84af73a8685988dc0a
diclementef92 <diclementef92@gmail.com>
2023-05-09 15:35:35 +0200

    first commit create maven project

---
+++ BackEnd/StayHealth/src/main/java/com/project/stayhealth/StayHealthApplication.java
@@ -0,0 +1,13 @@
+package com.project.stayhealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StayHealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StayHealthApplication.class, args);
+	}
+
+}
---
+++ BackEnd/StayHealth/src/test/java/com/project/stayhealth/StayHealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.stayhealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StayHealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
