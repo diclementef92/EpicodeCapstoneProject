36bc12a74236c471a089dae09be3375f4f6e869d
diclementef92 <diclementef92@gmail.com>
2023-05-28 17:27:03 +0200

    nav bar links and logo

--- front-end/src/components/Dashboard.jsx
+++ front-end/src/components/Dashboard.jsx
@@ -1,17 +1,10 @@
 import { useEffect, useState } from "react";
-import {
-  Button,
-  Card,
-  Container,
-  Navbar,
-  OverlayTrigger,
-  Tooltip,
-} from "react-bootstrap";
-import { Link, Navigate, useNavigate } from "react-router-dom";
-import { FetchUser } from "../hooks/FetchUser";
 import { useDispatch, useSelector } from "react-redux";
+
+import { Link, Navigate, useNavigate } from "react-router-dom";
+
 import DailyCalories from "./DailyCalories";
-import { BiLogOut } from "react-icons/bi";
+import MyNavbar from "./MyNavbar";
 
 const Dashboard = () => {
   const userDto = useSelector((state) => state.userDto);
@@ -22,37 +15,9 @@ const Dashboard = () => {
     console.log(userDto);
   }, []);
 
-  const renderTooltip = (props) => (
-    <Tooltip id="button-tooltip" {...props}>
-      logout
-    </Tooltip>
-  );
-
-  const handleLogOut = () => {
-    dispatch({ type: "LOGOUT" });
-    navigate("/");
-  };
-
   return (
     <>
-      <Navbar>
-        <Container>
-          <Navbar.Brand href="./">StayHealth</Navbar.Brand>
-          <Navbar.Toggle />
-          <Navbar.Collapse className="justify-content-end">
-            <Navbar.Text>{userDto.username}</Navbar.Text>
-            <OverlayTrigger
-              placement="right"
-              delay={{ show: 250, hide: 400 }}
-              overlay={renderTooltip}
-            >
-              <a onClick={handleLogOut}>
-                <BiLogOut />
-              </a>
-            </OverlayTrigger>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      <MyNavbar />
     </>
   );
 };
---
+++ front-end/src/components/MyNavbar.jsx
@@ -0,0 +1,72 @@
+import { useEffect, useState } from "react";
+import {
+  Button,
+  Card,
+  Container,
+  Image,
+  Nav,
+  NavLink,
+  Navbar,
+  OverlayTrigger,
+  Tooltip,
+} from "react-bootstrap";
+import { Link, Navigate, useNavigate } from "react-router-dom";
+import { FetchUser } from "../hooks/FetchUser";
+import { useDispatch, useSelector } from "react-redux";
+import DailyCalories from "./DailyCalories";
+import { BiLogOut } from "react-icons/bi";
+import logo from "../assets/logo.png";
+
+const MyNavbar = () => {
+  const userDto = useSelector((state) => state.userDto);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    console.log(userDto);
+  }, []);
+
+  const renderTooltip = (props) => (
+    <Tooltip id="button-tooltip" {...props}>
+      logout
+    </Tooltip>
+  );
+
+  const handleLogOut = () => {
+    dispatch({ type: "LOGOUT" });
+    navigate("/");
+  };
+
+  return (
+    <>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="./">
+            <Image src={logo} width={40} className="me-4" />
+            StayHealth
+          </Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="d-flex justify-content-between">
+            <Nav>
+              <Nav.Link href="./dashboard">DashBoard</Nav.Link>
+              <Nav.Link href="./myprofile">My Profile</Nav.Link>
+              <Nav.Link href="./weight">Weight Measurements</Nav.Link>
+            </Nav>{" "}
+          </Navbar.Collapse>
+          <div>
+            {userDto.username}{" "}
+            <OverlayTrigger
+              placement="right"
+              delay={{ show: 250, hide: 400 }}
+              overlay={renderTooltip}
+            >
+              <BiLogOut onClick={handleLogOut} style={{ cursor: "pointer" }} />
+            </OverlayTrigger>
+          </div>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+export default MyNavbar;
